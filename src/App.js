a41d8246335385a159b21f36bd90d6e503c89b32
yazid adem <yazid@A-2200262.AL-MOURADIA.DZ>
2022-12-25 22:26:49 +0100

    install Yup and ErrorMessage Validation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React , {Component} from "react";
-import { Formik , Field } from "formik";
-// Controller component
+import { Formik , Field , ErrorMessage  } from "formik";
+import * as Yup from 'yup';
 
 class App  extends Component {
      
@@ -8,17 +8,24 @@ class App  extends Component {
       console.log(values);
   }
   
+  
   formFunction = (props) => {
      return (
        <form onSubmit={props.handleSubmit}>
             <label>Name</label>
             <Field name="name" />
             <br/>
+            <ErrorMessage name="name" />
+            <br/>
             <label>Email</label>
             <Field name="email"  type="email" />
             <br/>
+            <ErrorMessage name="email" />
+            <br/>
             <label> Situation</label>
             <Field name="situation"  type="checkbox" />
+            <br/>
+            <ErrorMessage name="situation" />
             <br/>
             <label>Category</label>
             <Field name="category"  component="select" > 
@@ -26,12 +33,24 @@ class App  extends Component {
                <option value="category2">Category 2</option>
                <option value="category3">Category 3</option>
             </Field>
+            <br/>
+            <ErrorMessage name="category" />
+            <br/>
 
             <button type="submit">Send</button>
        </form>
      )
   }
-
+   
+  schema = () => {
+      const schema =  Yup.object().shape({
+        name : Yup.string().required(),
+        email : Yup.string().required(),
+        situation : Yup.string().required(),
+        category : Yup.string().required(),
+      });
+      return schema ;
+  }
   
    render () {
      
@@ -41,6 +60,7 @@ class App  extends Component {
              initialValues={{ name : '' , email : '' , situation : 'true' , category : ''}} 
              onSubmit={this.onSubmitFunction}
              render={this.formFunction}
+             validationSchema = {this.schema()}
            />
             
            
